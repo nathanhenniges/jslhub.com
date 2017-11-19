54eb359b465ee27193837b4cd49f9a6d87e940bb
Nathan Henniges <demonwolf@demonwolfdev.com>
2017-11-19 04:43:58 -0600

    Added image for hero and imported its SASS. Also added JS for the logo to change on scroll. Added dark and light logo.

--- assets/_js/main.js
+++ assets/_js/main.js
@@ -1,36 +1,36 @@
 // Fixs the menu so when link is clicked it would close the menu.
 $(function () {
 
-    $('#bs-example-navbar-collapse-1')
-        .on('shown.bs.collapse', function () {
-            $('#navbar-hamburger').addClass('hidden');
-            $('#navbar-close').removeClass('hidden');
-        })
-        .on('hidden.bs.collapse', function () {
-            $('#navbar-hamburger').removeClass('hidden');
-            $('#navbar-close').addClass('hidden');
-        });
+  $('#bs-example-navbar-collapse-1')
+    .on('shown.bs.collapse', function () {
+      $('#navbar-hamburger').addClass('hidden');
+      $('#navbar-close').removeClass('hidden');
+    })
+    .on('hidden.bs.collapse', function () {
+      $('#navbar-hamburger').removeClass('hidden');
+      $('#navbar-close').addClass('hidden');
+    });
 
 });
 
 $(document).on('click', '.navbar-collapse.in', function (e) {
-    if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
-        $(this).collapse('hide');
-    }
+  if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+    $(this).collapse('hide');
+  }
 });
 $(function () {
-    $(document).click(function (event) {
-        $('.navbar-collapse').collapse('hide');
-    });
+  $(document).click(function (event) {
+    $('.navbar-collapse').collapse('hide');
+  });
 });
 
 $(document).ready(function () {
-    setTimeout(function () {
-        $("#cookieConsent").fadeIn(200);
-    }, 4000);
-    $("#closeCookieConsent, .cookieConsentOK").click(function () {
-        $("#cookieConsent").fadeOut(200);
-    });
+  setTimeout(function () {
+    $("#cookieConsent").fadeIn(200);
+  }, 4000);
+  $("#closeCookieConsent, .cookieConsentOK").click(function () {
+    $("#cookieConsent").fadeOut(200);
+  });
 });
 
 $(document).ready(function () {
@@ -41,9 +41,11 @@ $(document).ready(function () {
     $(document).scroll(function () {
       scroll_start = $(this).scrollTop();
       if (scroll_start > offset.top) {
-        $('.navbar-default').addClass('navbar-scroll');
+        $(".navbar-default").addClass('navbar-scroll');
+        $('.navbar-brand img').attr('src', '../assets/images/logo_text_dark.png');
       } else {
         $(".navbar-default").removeClass('navbar-scroll');
+        $('.navbar-brand img').attr('src', '../assets/images/logo_text_light.png');
       }
     });
   }
